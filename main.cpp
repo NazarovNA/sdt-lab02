a03802747fc3f9850b640de114e2f7de9e766c9d
u211-14 <u211-14@public.mpei.local>
2018-03-20 10:31:11 +0300

    dop zadanie

--- main.cpp
+++ main.cpp
@@ -6,10 +6,5 @@
  int a, b;
  cout << "Enter A and B: ";
  cin >> a >> b;
- cout << "A + B = " << a + b << '\n'
- << "A * 2 = " << a * 2 << '\n'
- << "A - B = " << a - b << '\n'
-
- << "A * B = " << a * b << '\n'
- << "A/B = " << a / b << '\n';
+ cout << "zadanie vipolneno";
  }
